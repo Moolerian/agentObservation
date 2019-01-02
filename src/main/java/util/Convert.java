467654edfa7340478fab6b45f2b9020fc4fd3f31
Moolerian <Moolerian@Gmail.com>
2019-01-03 03:07:25 +0330

    commit

--- src/main/java/util/Convert.java
+++ src/main/java/util/Convert.java
@@ -1,7 +1,26 @@
 package util;
 
+import model.User;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Convert {
 
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+//        User user = new User();
+//        user.setUserName("asghar");
+//        user.setPassword("aliiiiiiii");
+//        mapper.writeValue(new File("c:\\file.json"), user);
 
 
+        User obj = mapper.readValue(new File("c:\\file.json"), User.class);
+
+        System.out.println(obj);
+
+
+    }
 }
